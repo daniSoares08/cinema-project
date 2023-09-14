2a579a10870bbb3cfa0d7092350b8668bc7860c1
MuriloBertelli <mrlbertelli@gmail.com>
2023-09-14 16:30:21 -0300

    College project

--- cinema.c
+++ cinema.c
@@ -1,32 +1,54 @@
 #include <stdio.h>
 #define FILAS 14
-#define POLTRONAS 10 
+#define POLTRONAS 10
 
-char plateia[FILAS][POLTRONAS]; 
+char plateia[FILAS][POLTRONAS];
 
-void incializarLugares(){
-    int i,j;
-    for (i = 0 ; i < FILAS; i++){
-        for (int j = 0; j < POLTRONAS; j++){
+void incializarLugares()
+{
+    int i, j;
+    for (i = 0; i < FILAS; i++)
+    {
+        for (int j = 0; j < POLTRONAS; j++)
+        {
             plateia[i][j] = '-';
         }
     }
 }
 
-void mostraPlateia(){
+void mostraPlateia()
+{
     printf("LUGARES:\n");
-    for (int i = 0; i < FILAS; i++){
-        for (int j = 0; j < POLTRONAS; j++){
+    for (int i = 0; i < FILAS; i++)
+    {
+        for (int j = 0; j < POLTRONAS; j++)
+        {
             printf("%4c", plateia[i][j]);
         }
         printf("\n");
     }
 }
 
-int main(){
+int main()
+{
     incializarLugares();
-    mostraPlateia();
-    return 0; 
 
+    printf("------CINEMA------");
 
+    while (1)
+    {
+        printf("0 - Sair\n");
+        printf("1 - ender ingresso\n");
+        printf("2 - Mostrar plateia\n");
+        printf("3 - Mostrar ocupação\n");
+
+        int opcao;
+        prinf("Qual sua opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+            // Continuar as opções de cada valor
+        }
+    }
 }
