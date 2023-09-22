8bbfb84e593c7c0a856d6f2966836c82065c9551
MuriloBertelli <mrlbertelli@gmail.com>
2023-09-22 15:29:31 -0300

    Cinema

--- cinema.c
+++ cinema.c
@@ -3,8 +3,8 @@
 #define POLTRONAS 10
 
 char plateia[FILAS][POLTRONAS];
-int vendidosMeia = 0; // Contador começando em 0 
-int vendidosInteira = 0; // Contador começando em 0 
+int vendidosMeia = 0;    // Contador começando em 0
+int vendidosInteira = 0; // Contador começando em 0
 
 void incializarLugares()
 {
@@ -31,60 +31,75 @@ void mostraPlateia()
     }
 }
 
-void venderIngresso(){
+void venderIngresso()
+{
 
     mostraPlateia();
     int poltrona;
 
-    while (1){
+    while (1)
+    {
         printf("Escolha sua poltrona <1..%d>: ", FILAS * POLTRONAS);
-        scanf("%d" , &poltrona);
+        scanf("%d", &poltrona);
 
-        if (poltrona < 1 || poltrona > FILAS * POLTRONAS){
+        if (poltrona < 1 || poltrona > FILAS * POLTRONAS)
+        {
             printf("Poltrona que você selecionou não existe, porfavor escolha no intervado de <1..%d>. Tente novamente.", FILAS * POLTRONAS);
             continue;
         }
-        
+
         int linha = (poltrona - 1) / POLTRONAS;
         int coluna = (poltrona - 1) / POLTRONAS;
 
-        if (plateia [linha][coluna] == '-'){
-            
+        if (plateia[linha][coluna] == '-')
+        {
+
             char tipo;
             printf("Qual o tipo, <m>eia ou <i>nteira: ");
             scanf(" %c", &tipo);
 
-            if (tipo == 'm'|| tipo == 'i'){
+            if (tipo == 'm' || tipo == 'i')
+            {
                 plateia[linha][coluna] = tipo;
                 printf("Compra feita com sucesso!\n");
 
-                if (tipo == 'm'){
+                if (tipo == 'm')
+                {
                     vendidosMeia++;
-                } else {
+                }
+                else
+                {
                     vendidosInteira++;
                 }
-            } else {
+            }
+            else
+            {
                 printf("Tipo de ingresso invalido. Escolha 'm' para meia e 'i' para inteira\n");
             }
-        }   else {
+        }
+        else
+        {
             printf("Poltrona Já ocupada. Escolha outra.\n");
-
         }
     }
 }
 
-
-void mostrarOcupacao(){
+void mostrarOcupacao()
+{
     int totalIngressos = FILAS * POLTRONAS;
     int ocupados = vendidosMeia = vendidosInteira;
     int livres = totalIngressos - ocupados;
 
-    for (int i = 0; i < FILAS; i++){
-        for (int j = 0; j < POLTRONAS; j++){
-            if (plateia[i][j] == 'm' || plateia[i][j] == 'i'){
+    for (int i = 0; i < FILAS; i++)
+    {
+        for (int j = 0; j < POLTRONAS; j++)
+        {
+            if (plateia[i][j] == 'm' || plateia[i][j] == 'i')
+            {
                 printf("%2c ", plateia[i][j]);
             }
-            else{
+            else
+            {
                 printf("_ ");
             }
         }
@@ -93,7 +108,7 @@ void mostrarOcupacao(){
 
     printf("Plateia possui %d lugares\n", totalIngressos);
     printf("Foram vendidos %d ingressos, sendo:\n", ocupados);
-    printf("%d - meias\n",vendidosMeia);
+    printf("%d - meias\n", vendidosMeia);
     printf("%d - inteiras\n", vendidosInteira);
 }
 
@@ -101,7 +116,7 @@ int main()
 {
     incializarLugares();
 
-    printf("------CINEMA------");
+    printf("------CINEMA------\n");
 
     while (1)
     {
@@ -114,18 +129,21 @@ int main()
         printf("Qual sua opção: ");
         scanf("%d", &opcao);
 
-        switch (opcao){
-            case 0:
-                printf("Obrigado por usar o nosso sistema!\n");
-                return 0;
-            case 1:
-                venderIngresso();
-                break;
-            case 3:
-                mostrarOcupacao();
-                break;
-            default:
-                printf("Opção invalida. Escolha entre 0, 1, ou 3.\n");  
+        switch (opcao)
+        {
+        case 0:
+            printf("Obrigado por usar o nosso sistema!\n");
+            return 0;
+        case 1:
+            venderIngresso();
+            break;
+        case 2:
+            mostraPlateia();
+            break;
+        case 3:
+            mostrarOcupacao();
+        default:
+            printf("Opção invalida. Escolha entre 0, 1, 2 ou 3.\n");
         }
     }
     return 0;
